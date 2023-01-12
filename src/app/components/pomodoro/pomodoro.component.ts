240ae913beee15680dee6b127e393d5179f58303
gabrielamonaro <gabriela.monaro@aluno.ufabc.edu.brgit config --global user.email gabriela.monaro@aluno.ufabc.edu.br>
2023-01-12 19:09:45 -0300

    Estilização da página principal - timer podomoro

--- src/app/components/pomodoro/pomodoro.component.ts
+++ src/app/components/pomodoro/pomodoro.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pomodoro',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pomodoro.component.css']
 })
 export class PomodoroComponent implements OnInit {
-
+  @Input() dots:number[] = [10,3,60,85]
 
   constructor() { }
 

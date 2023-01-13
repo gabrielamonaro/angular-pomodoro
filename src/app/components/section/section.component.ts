4d2c986d8ccb70aecc3fb023f5462eafc0492332
gabrielamonaro <gabriela.monaro@aluno.ufabc.edu.brgit config --global user.email gabriela.monaro@aluno.ufabc.edu.br>
2023-01-12 23:36:00 -0300

    Criação de um arquivo json e passagem de valores para componentes.

--- src/app/components/section/section.component.ts
+++ src/app/components/section/section.component.ts
@@ -1,25 +1,28 @@
 import { Component, Input, OnInit } from '@angular/core';
-
+import sections_data from "../../../assets/data/sections_data.json"
 @Component({
+
   selector: 'app-section',
   templateUrl: './section.component.html',
   styleUrls: ['./section.component.css']
 })
 export class SectionComponent implements OnInit {
-  @Input() visibility:boolean = false;
-
+  @Input() visibility:boolean[] = [false,false,false,false];
+  sections = sections_data;
+ 
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
-  changeVisibility(){
-   if(this.visibility == false)
+  changeVisibility(id: number){
+   if(this.visibility[id] == false)
     {
-      this.visibility = true;
+      this.visibility[id] = true;
     }
     else{
-      this.visibility = false;
+      this.visibility[id] = false;
     }
   }
 
